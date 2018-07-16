a5ba06bc83221abf8a66e12e0d457de016db5655
Kalan <kjj6198@gmail.com>
2018-07-17 00:05:07 +0800

    add basic AdaGrad optimizer.

--- utils/optimizers.py
+++ utils/optimizers.py
@@ -1,4 +1,25 @@
+import numpy as np
+
 """
 introduce some useful optimizers.
 AdaGrad, Momentum
 """
+
+
+class AdaGrad:
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
+        self.h = {}
+
+    def update(self, weights, grads, zero_constant=1e-7):
+        if zero_constant == 0:
+            raise ZeroDivisionError("can not divide by zero.")
+        for k, v in weights.items():
+            self.h[k] = np.zeros_like(v)
+
+        for key in weights.keys():
+            self.h[k] += grads[k] * grads[k]
+            weights[k] -= (
+                self.learning_rate * grads[key] / (np.sqrt(self.h[key]) + zero_constant)
+            )
+
